a5b17300e84a0426b4e7505e1fd354de31c30e35
lllf <littlelightlittlefire@gmail.com>
2015-08-18 19:14:50 +1000

    Works for Google images, videos, news, etc

--- content.js
+++ content.js
@@ -1,26 +1,25 @@
-// Inject code into the page to access the underlying JavaScript objects
-// https://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
 var actualCode = '(' + function() {
     'use strict';
 
     // Detect Google search page, same technique used here:
-    // https://github.com/palant/searchlinkfix/blob/master/lib/content.js#L72
-    function isSearch() {
+    function googleProduct() {
         var google = window.google;
-        return !(google && (google.sn || google.search));
+        return google ? google.sn : null;
     }
 
     var found = null;
 
     // Fix the links by adding a pair of mousedown handlers
-    window.addEventListener('mousedown', function(event) {
-        if (!isSearch(window)) {
+    window.addEventListener('mousedown', function(e) {
+        var product = googleProduct();
+        if (!product) {
             return;
         }
 
         // Find the <a> tag
-        for (var curr = event.target;
-                curr && curr.localName.toLowerCase() !== 'a';
+        var curr = null;
+        for (curr = e.target;
+                curr && curr.localName && curr.localName.toLowerCase() !== 'a';
                 curr = curr.parentNode) {
         }
 
@@ -30,22 +29,30 @@ var actualCode = '(' + function() {
             return;
         }
 
+        // Stop the propagation if it is images, since Google images stops the propagation
+        if (product === 'images') {
+            e.stopPropagation();
+            return;
+        }
+
         // Save the link location
         found = {
             'href': curr.href,
             'element': curr
         };
-
     }, true);
 
     window.addEventListener('mousedown', function() {
-        if (found) {
+        if (found && found.element.getAttribute('data-href') === found.href) {
             found.element.href = found.href;
         }
     }, false);
 
 } + ')();';
+
+// Inject code into the page to access the underlying JavaScript objects
+// https://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
 var script = document.createElement('script');
 script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
+(document.head || document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
